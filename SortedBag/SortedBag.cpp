6a11effca108ae6e8de13af4bacd5074b109cd63
Marian Guceanu <george.guceanu@stud.ubbcluj.ro>
2022-06-01 19:14:59 +0300

    added lab 4 and 5 (multimap and sored bag)

--- SortedBag/SortedBag.cpp
+++ SortedBag/SortedBag.cpp
@@ -1,8 +1,9 @@
 #include "SortedBag.h"
 #include "SortedBagIterator.h"
 
-SortedBag::SortedBag(Relation r) {
-	//TODO - Implementation
+SortedBag::SortedBag(Relation r)
+{
+    // TODO - Implementation
     relation = r;
     length = 0;
     tree = nullptr;
@@ -13,9 +14,10 @@ SortedBag::SortedBag(Relation r) {
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-void SortedBag::add(TComp e) {
-	//TODO - Implementation
-    if(tree == nullptr)
+void SortedBag::add(TComp e)
+{
+    // TODO - Implementation
+    if (tree == nullptr)
         tree = createNode(e);
     else
         addValueToTree(tree, e);
@@ -27,49 +29,66 @@ void SortedBag::add(TComp e) {
  * Worst case: Theta(log length)
  * Overall: Theta(log length)
  */
-bool SortedBag::remove(TComp e) {
-	//TODO - Implementation
-	auto nodeToRemove = findNode(tree, e);
-    if(nodeToRemove == nullptr)
+bool SortedBag::remove(TComp e)
+{
+    // TODO - Implementation
+    auto nodeToRemove = findNode(tree, e);
+    if (nodeToRemove == nullptr)
         return false;
     auto parent = findParent(tree, e);
-    if(length == 1)
+    if (length == 1)
         removeNode(tree);
-    else if(parent == nullptr && (nodeToRemove->left == nullptr || nodeToRemove->right == nullptr)){
-        if(nodeToRemove->left == nullptr){
+    else if (parent == nullptr && (nodeToRemove->left == nullptr || nodeToRemove->right == nullptr))
+    {
+        if (nodeToRemove->left == nullptr)
+        {
             auto node = nodeToRemove->right;
             removeNode(tree);
             tree = node;
-        }else{
+        }
+        else
+        {
             auto node = nodeToRemove->left;
             removeNode(tree);
             tree = node;
         }
-    }else if(nodeToRemove->left == nullptr && nodeToRemove->right == nullptr){
-        if(relation(nodeToRemove->value, parent->value))
+    }
+    else if (nodeToRemove->left == nullptr && nodeToRemove->right == nullptr)
+    {
+        if (relation(nodeToRemove->value, parent->value))
             removeNode(parent->left);
         else
             removeNode(parent->right);
-    }else if(nodeToRemove->left == nullptr && nodeToRemove->right != nullptr){
-        if(relation(nodeToRemove->value, parent->value))
+    }
+    else if (nodeToRemove->left == nullptr && nodeToRemove->right != nullptr)
+    {
+        if (relation(nodeToRemove->value, parent->value))
             parent->left = nodeToRemove->right;
         else
             parent->right = nodeToRemove->right;
         removeNode(nodeToRemove);
-    }else if(nodeToRemove->left != nullptr && nodeToRemove->right == nullptr){
-        if(relation(nodeToRemove->value, parent->value))
-            parent->left=nodeToRemove->left;
+    }
+    else if (nodeToRemove->left != nullptr && nodeToRemove->right == nullptr)
+    {
+        if (relation(nodeToRemove->value, parent->value))
+            parent->left = nodeToRemove->left;
         else
             parent->right = nodeToRemove->left;
         removeNode(nodeToRemove);
-    }else{
+    }
+    else
+    {
         auto maxNode = nodeToRemove->left;
-        if(maxNode->right == nullptr){
+        if (maxNode->right == nullptr)
+        {
             nodeToRemove->left = maxNode->left;
             removeNode(maxNode);
-        }else{
-            Node* previousMaxNode = nullptr;
-            while(maxNode->right != nullptr){
+        }
+        else
+        {
+            Node *previousMaxNode = nullptr;
+            while (maxNode->right != nullptr)
+            {
                 previousMaxNode = maxNode;
                 maxNode = maxNode->right;
             }
@@ -86,9 +105,10 @@ bool SortedBag::remove(TComp e) {
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-bool SortedBag::search(TComp elem) const {
-	//TODO - Implementation
-	return doesContain(tree, elem);
+bool SortedBag::search(TComp elem) const
+{
+    // TODO - Implementation
+    return doesContain(tree, elem);
 }
 
 /*
@@ -96,39 +116,42 @@ bool SortedBag::search(TComp elem) const {
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-int SortedBag::nrOccurrences(TComp elem) const {
-	//TODO - Implementation
-	auto count = 0;
+int SortedBag::nrOccurrences(TComp elem) const
+{
+    // TODO - Implementation
+    auto count = 0;
     countValue(findNode(tree, elem), elem, count);
     return count;
 }
 
-
 // O(1)
-int SortedBag::size() const {
-	//TODO - Implementation
-	return length;
+int SortedBag::size() const
+{
+    // TODO - Implementation
+    return length;
 }
 
 // O(1)
-bool SortedBag::isEmpty() const {
-	//TODO - Implementation
+bool SortedBag::isEmpty() const
+{
+    // TODO - Implementation
     return length < 1;
 }
 
-
-SortedBagIterator SortedBag::iterator() const {
-	return SortedBagIterator(*this);
+SortedBagIterator SortedBag::iterator() const
+{
+    return SortedBagIterator(*this);
 }
 
-
-SortedBag::~SortedBag() {
-	//TODO - Implementation
+SortedBag::~SortedBag()
+{
+    // TODO - Implementation
     destroyTree(tree);
 }
 
 // O(1)
-Node* SortedBag::createNode(int value, Node *left, Node *right) {
+Node *SortedBag::createNode(int value, Node *left, Node *right)
+{
     auto node = new Node;
     node->value = value;
     node->left = left;
@@ -140,17 +163,27 @@ Node* SortedBag::createNode(int value, Node *left, Node *right) {
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-void SortedBag::addValueToTree(Node *root, TComp value) {
-    if(relation(value, root->value)){
-        if(root->left == nullptr){
+void SortedBag::addValueToTree(Node *root, TComp value)
+{
+    if (relation(value, root->value))
+    {
+        if (root->left == nullptr)
+        {
             root->left = createNode(value);
-        }else{
+        }
+        else
+        {
             addValueToTree(root->left, value);
         }
-    }else{
-        if(root->right == nullptr){
+    }
+    else
+    {
+        if (root->right == nullptr)
+        {
             root->right = createNode(value);
-        }else{
+        }
+        else
+        {
             addValueToTree(root->right, value);
         }
     }
@@ -161,12 +194,13 @@ void SortedBag::addValueToTree(Node *root, TComp value) {
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-bool SortedBag::doesContain(Node *root, TComp value) const {
-    if(root == nullptr)
+bool SortedBag::doesContain(Node *root, TComp value) const
+{
+    if (root == nullptr)
         return false;
-    if(root->value == value)
+    if (root->value == value)
         return true;
-    if(relation(value, root->value))
+    if (relation(value, root->value))
         return doesContain(root->left, value);
     return doesContain(root->right, value);
 }
@@ -176,47 +210,53 @@ bool SortedBag::doesContain(Node *root, TComp value) const {
  * Worst case: Theta (log length)
  * Overall: O(log length)
  */
-Node* SortedBag::findParent(Node *root, TComp value) const {
-    if(root->value == value)
+Node *SortedBag::findParent(Node *root, TComp value) const
+{
+    if (root->value == value)
         return nullptr;
-    if(relation(value, root->value)){
-        if(root->left == nullptr)
+    if (relation(value, root->value))
+    {
+        if (root->left == nullptr)
             return nullptr;
-        if(root->left->value == value)
+        if (root->left->value == value)
             return root;
         return findParent(root->left, value);
     }
-    if(root->right == nullptr)
+    if (root->right == nullptr)
         return nullptr;
-    if(root->right->value == value)
+    if (root->right->value == value)
         return root;
     return findParent(root->right, value);
 }
 
-void SortedBag::filter(Condition cond) {
+void SortedBag::filter(Condition cond)
+{
     int iterated;
     auto stop = false;
     TComp current;
-    iterator().iterateTree(tree, [&](Node* node){
+    iterator().iterateTree(
+        tree, [&](Node *node)
+        {
         current = node->value;
         if(!cond(current))
             remove(current);
         iterated++;
         if(iterated == length)
-            stop = true;
-    }, stop);
+            stop = true; },
+        stop);
 }
 /*
  * Best case: Theta(1)
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-Node* SortedBag::findNode(Node *root, TComp value) const {
-    if(root == nullptr)
+Node *SortedBag::findNode(Node *root, TComp value) const
+{
+    if (root == nullptr)
         return nullptr;
-    if(root->value == value)
+    if (root->value == value)
         return root;
-    if(relation(value, root->value))
+    if (relation(value, root->value))
         return findNode(root->left, value);
     return findNode(root->right, value);
 }
@@ -226,11 +266,13 @@ Node* SortedBag::findNode(Node *root, TComp value) const {
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-Node* SortedBag::findMin(Node *root) {
-    if(root == nullptr){
+Node *SortedBag::findMin(Node *root)
+{
+    if (root == nullptr)
+    {
         return nullptr;
     }
-    if(root->left == nullptr)
+    if (root->left == nullptr)
         return root;
     return findMin(root->left);
 }
@@ -240,29 +282,31 @@ Node* SortedBag::findMin(Node *root) {
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-void SortedBag::countValue(Node *root, TComp value, int &count) const {
-    if(root == nullptr)
+void SortedBag::countValue(Node *root, TComp value, int &count) const
+{
+    if (root == nullptr)
         return;
-    if(root->value == value)
+    if (root->value == value)
         count++;
-    if(relation(value, root->value))
+    if (relation(value, root->value))
         countValue(root->left, value, count);
     countValue(root->right, value, count);
 }
-//O(1)
-void SortedBag::removeNode(Node *&node) {
+// O(1)
+void SortedBag::removeNode(Node *&node)
+{
     delete node;
     node = nullptr;
 }
-
 
 /*
  * Best case: Theta(1)
  * Worst case: Theta(log length)
  * Overall: O(log length)
  */
-void SortedBag::destroyTree(Node *root) {
-    if(root == nullptr)
+void SortedBag::destroyTree(Node *root)
+{
+    if (root == nullptr)
         return;
     destroyTree(root->left);
     destroyTree(root->right);
